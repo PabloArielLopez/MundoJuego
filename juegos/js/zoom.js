ceea2fc7b3f066f1ad447f5b91e973e93e2cea3f
PabloArielLopez <pablolopez.tr@hotmail.com>
2017-09-30 14:55:56 -0300

    Add files via upload

--- juegos/js/zoom.js
+++ juegos/js/zoom.js
@@ -1,1 +1,18 @@
+var globalZoom=500;
 
+function zoomUp(){
+  var Upzoom = document.getElementById('game');
+  if (globalZoom<800) {
+    globalZoom=globalZoom+50;
+  Upzoom.style.height = globalZoom+"px";
+  }
+
+}
+
+function zoomDown(){
+  var Downzoom = document.getElementById('game');
+  if (globalZoom>200) {
+    globalZoom=globalZoom-50;
+    Downzoom.style.height = globalZoom+"px";
+  }
+}
